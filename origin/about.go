f306056b5eda3fc97144246d8072aabe7e94574a
Matcha <matcha12@live.com>
2020-07-12 05:11:36 +0800

    about接口

---
+++ origin/about.go
@@ -0,0 +1,37 @@
+package origin
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"io/ioutil"
+	"net/http"
+)
+
+type RespAbout struct {
+	Code         int    `json:"code"`
+	ErrorMessage string `json:"errorMessage"`
+	Data         struct {
+		Version string `json:"version"`
+	} `json:"data"`
+}
+
+func About(addr string) (RespAbout, error) {
+	resp, err := http.DefaultClient.Get("http://" + addr + "/about")
+	if err != nil {
+		return RespAbout{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return RespAbout{}, err
+	}
+
+	var respAbout RespAbout
+	err = jsoniter.Unmarshal(body, &respAbout)
+	if err != nil {
+		return RespAbout{}, err
+	}
+
+	err = CheckErrorCode(respAbout.Code)
+	return respAbout, err
+}
